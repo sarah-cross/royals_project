bfa3f6eee7566bd355e1afb1cdb4d66f11e5a4b8
Sarah Cross <scc0079@auburn.edu>
2025-02-20 08:05:24 -0800

    Removed SSR for a simpler CSR setup

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,22 +1,16 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-// Angular Material Modules
-import { MatTableModule } from '@angular/material/table';
-import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSelectModule } from '@angular/material/select';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideHttpClient(withFetch()),
-    provideClientHydration(withEventReplay()), provideAnimationsAsync(),
-    importProvidersFrom(MatTableModule, MatSortModule, MatPaginatorModule, MatSelectModule)
+    provideAnimationsAsync(),
   ]
 };
